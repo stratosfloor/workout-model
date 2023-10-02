d20b427c9ab37bad81af09182e2cf0e298c60c79
Emil Noren <stratosfloor@hotmail.com>
2023-10-02 07:35:40 +0200

    Implemente repository for exercise

---
+++ lib/src/repositories/exercise_repository.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+import 'package:workout_model/src/models/exercise_model.dart';
+import 'package:hive/hive.dart';
+
+class ExerciseRepository {
+  final List<Exercise> _exercises = [];
+  late Box<String> _exerciseBox;
+
+  ExerciseRepository() {
+    Directory directory = Directory.current;
+    Hive.init(directory.path);
+    // Hive.initFlutter(directory.path);
+  }
+
+  Future<void> initalize() async {
+    if (!Hive.isBoxOpen('exercises')) {
+      _exerciseBox = await Hive.openBox('exercises');
+    }
+    print('Box is open');
+  }
+
+  bool create(Exercise exercise) {
+    if (!Hive.isBoxOpen('exercises')) {
+      // _exerciseBox =  Hive.openBox('exercises');
+      throw StateError('Please await ExerciseRepository initalize method');
+    }
+    var existingExercise = _exerciseBox.get(exercise.id);
+    if (existingExercise != null) {
+      return false;
+    }
+    _exerciseBox.put(exercise.id, exercise.serialize());
+    return true;
+  }
+
+  Exercise? read(String id) {
+    var serialized = _exerciseBox.get(id);
+    return serialized != null ? Exercise.deserialize(serialized) : null;
+  }
+
+  Exercise update({
+    required Exercise exercise,
+    String? name,
+    String? description,
+    int? repetitions,
+    int? restTime,
+    int? sets,
+    double? weight,
+  }) {
+    int index = _exercises.indexWhere((e) => e.id == exercise.id);
+    if (index == -1) {
+      throw Exception('Exercise not found');
+    }
+    final newEx = Exercise(
+      name: name ?? exercise.name,
+      description: description ?? exercise.description,
+      repetitions: repetitions ?? exercise.repetitions,
+      restTime: restTime ?? exercise.restTime,
+      sets: sets ?? exercise.sets,
+      weight: weight ?? exercise.weight,
+    );
+    _exercises[index] = newEx;
+    return _exercises[index];
+  }
+
+  bool delete(String id) {
+    int index = _exercises.indexWhere((r) => r.id == id);
+    if (index >= 0) {
+      _exercises.removeAt(index);
+      return true;
+    }
+    return false;
+  }
+
+  List<Exercise> list() => _exerciseBox.values
+      .map((serialized) => Exercise.deserialize(serialized))
+      .toList();
+}
